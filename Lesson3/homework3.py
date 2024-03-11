cf6b25619d2c173880dc90eb44273e87b470e40f
Anna Tokmakova <ann.tokmakova22@gmail.com>
2024-03-11 18:02:32 +0200

    added homework3

---
+++ Lesson3/homework3.py
@@ -0,0 +1,15 @@
+#calculator Tokmakova Anna
+operation = '+'
+num1 = 100
+num2 = 25
+
+if operation == '+':
+    print(num1+num2)
+elif operation == '-':
+    print(num1 - num2)
+elif operation == '*':
+    print(num1 * num2)
+elif operation == '/':
+    print(num1 / num2)
+else:
+    print("Undefined operation")
