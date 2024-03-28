2bbf45042de2cecdae94983ef3a0d9b1bea92067
Anna Tokmakova <ann.tokmakova22@gmail.com>
2024-03-28 14:11:48 +0200

    Homework after removing mistakes

--- Lesson7_Set/homework7_loops_and_set.py
+++ Lesson7_Set/homework7_loops_and_set.py
@@ -1,26 +1,11 @@
-# Task 1
+# # Task 1
 print('Task 1: \'Unique list\'')
-print('1 way - Discarding duplicates')
 first = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 second = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-unique_first = []
-unique_second = []
-for number1 in first:
-    if first.count(number1) == 1:
-        unique_first.append(number1)
-for number2 in second:
-    if second.count(number2) == 1:
-        unique_second.append(number2)
-first_set = set(unique_first)
-second_set = set(unique_second)
-numbers = first_set.intersection(second_set)
-print(list(numbers))
-
-print('2 way - Taking duplicates into account')
 unique_first = set(first)
 unique_second = set(second)
 unique_common_numbers = unique_first.intersection(unique_second)
-print(list(unique_common_numbers))
+print(f'List with unique common numbers it is: {list(unique_common_numbers)}.')
 
 # Task 2
 print('\nTask 2: \'Missing_elements\'')
@@ -29,7 +14,7 @@ second_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 first_set = set(first_list)
 second_set = set(second_list)
 missing_elements = first_set - second_set
-print(missing_elements)
+print(f'Elements that are absent in the second list: {missing_elements}.')
 
 # Task 3
 print('\nTask 3: \'Unique numbers\'')
@@ -52,13 +37,12 @@ if unique_numbers:
 else:
     print('This list contains only duplicate elements.')
 
-
 # Task 4
 print('\nTask 4: \'Minimum value\'')
 print('1 way')
 elements1 = [1, 5, -300, 0, 5, 10, 2, 68, 0]
 sorted_set = sorted(set(elements1))
-print(sorted_set[0])
+print(f'Mimimum value it is: {sorted_set[0]}.')
 
 print('2 way')
 elements2 = [1, 5, -300, 0, 5, 10, 2, 68, 0]
@@ -66,7 +50,7 @@ min_element = elements2[0]
 for element in elements2:
     if element < min_element:
         min_element = element
-print(min_element)
+print(f'Mimimum value it is: {min_element}.')
 
 # Task 5
 print('\nTask 5: \'Sum of all elements\'')
@@ -74,5 +58,4 @@ numbers_for_addition = [1, 5, 50, 0, 5, 10, 2, 68]
 sum_of_numbers = 0
 for i in numbers_for_addition:
     sum_of_numbers += i
-    condition_for_addition = list[(numbers_for_addition.index(i) - 1)]
 print(f'After adding all elements from {numbers_for_addition}, we get {sum_of_numbers}.')
