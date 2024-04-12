c80438267ffb63a2cd593dc5aa2e86b459c1baac
Anna Tokmakova <ann.tokmakova22@gmail.com>
2024-04-12 19:14:43 +0300

    Added homework11

---
+++ Lesson11_Modules_and_Packages/classwork11/test1.py
@@ -0,0 +1,24 @@
+# 1-------------------------
+# import classwork11
+# import math
+# print(classwork11.hello('Test'))
+# print(math.sqrt(25))
+
+# 2--------------------------
+# from classwork11 import hello
+# print(hello('Lola'))
+
+# 3-------------------------
+# import classwork11 as cl
+# print(cl.hello('Lila'))
+
+# 4-------------------------
+# import classwork11 as cl
+# print(f'{__name__}')
+# print(cl.hello('Luna'))
+
+# 5------------------------
+def custom_sum_1(num1, num2):
+    return  num1 + num2
+
+
